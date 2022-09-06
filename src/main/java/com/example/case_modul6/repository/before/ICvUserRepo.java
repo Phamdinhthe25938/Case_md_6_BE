70fac6817749c3237b4a25f4bcb76b5da3dc8ced
Phamdinhthe250938 <phamdinhthe2509@gmail.com>
2022-09-06 08:35:13 +0700

    add tinh rasts

---
+++ src/main/java/com/example/case_modul6/repository/before/ICvUserRepo.java
@@ -0,0 +1,7 @@
+package com.example.case_modul6.repository.before;
+
+import com.example.case_modul6.model.before.CvUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ICvUserRepo extends CrudRepository<CvUser,Integer>{
+}
